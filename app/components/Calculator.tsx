1fd1b74c3fc47ad0de00bc4c7e73f8438c922646
Calvin Nordstrom <calnordstrom@icloud.com>
2025-03-09 16:44:07 -0400

    refactor: fixed display precision bug

--- app/components/Calculator.tsx
+++ app/components/Calculator.tsx
@@ -138,7 +138,7 @@ export default function Calculator({ config }: CalculatorProps) {
           )
         };
       } else {
-        input = newFieldData[key].real;
+        input = parsedValue;
       }
     });
     setFieldData(newFieldData);
