5f5865deabb8ec5b2aa38a1e61da6d51674339ea
Calvin Nordstrom <calnordstrom@icloud.com>
2025-03-28 20:54:07 -0400

    feat: added math calculators category page

---
+++ app/math/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import Link from 'next/link';
+import CalculatorLayout from '../components/CalculatorLayout';
+
+export const metadata: Metadata = {
+  title: 'Math Calculators',
+  description: 'Arithmecal math calculators',
+  keywords: '',
+}
+
+export default function MathCalculatorsPage() {
+  return (
+    <CalculatorLayout>
+      <h1>Math Calculators</h1>
+      <div className='calculator-category'>
+        <h2>Algebra Calculators</h2>
+        <ul>
+          <li><Link href='math/foil'>FOIL Calculator</Link></li>
+          <li><Link href='math/trinomial-factoring'>Trinomial Factoring Calculator</Link></li>
+          <li><Link href='math/percent-change'>Percent Change Calculator</Link></li>
+        </ul>
+
+        <h2>Geometry Calculators</h2>
+        <ul>
+          <li><Link href='math/circumscribed-circle'>Circumscribed Circle Calculator</Link></li>
+          <li><Link href='math/triangle-30-60-90'>30 60 90 Triangle Calculator</Link></li>
+        </ul>
+      </div>
+    </CalculatorLayout>
+  );
+}
