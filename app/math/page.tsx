37ab036680cb7fc9010eb69390c6618459bf4244
Calvin Nordstrom <calnordstrom@icloud.com>
2025-04-02 15:38:41 -0400

    refactor: updated categories
    Moved quadratic formula calculator to algebra calculators category

--- app/math/page.tsx
+++ app/math/page.tsx
@@ -18,12 +18,12 @@ export default function MathCalculatorsPage() {
           <li><Link href='math/foil'>FOIL Calculator</Link></li>
           <li><Link href='math/trinomial-factoring'>Trinomial Factoring Calculator</Link></li>
           <li><Link href='math/percent-change'>Percent Change Calculator</Link></li>
+          <li><Link href='math/quadratic-formula'>Quadratic Formula Calculator</Link></li>
         </ul>
 
         <h2>Geometry Calculators</h2>
         <ul>
           <li><Link href='math/circumscribed-circle'>Circumscribed Circle Calculator</Link></li>
-          <li><Link href='math/quadratic-formula'>Quadratic Formula Calculator</Link></li>
           <li><Link href='math/triangle-30-60-90'>30 60 90 Triangle Calculator</Link></li>
           <li><Link href='math/triangle-45-45-90'>45 45 90 Triangle Calculator</Link></li>
         </ul>
