7cf059cd7f306de29600d61e2b81ab8847e1323b
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 13:01:27 -0400

    feat: added unit definition

---
+++ cnconvert/units/apparentPower.ts
@@ -0,0 +1,29 @@
+import { UnitDefinition } from "../unit";
+
+export const apparentPowerUnits: Record<string, UnitDefinition> = {
+  VA: {
+    name: "volt-ampere",
+    toAnchor: 1,
+    dimension: "apparentPower"
+  },
+  mVA: {
+    name: "millivolt-ampere",
+    toAnchor: 0.001,
+    dimension: "apparentPower"
+  },
+  kVA: {
+    name: "kilovolt-ampere",
+    toAnchor: 1000,
+    dimension: "apparentPower"
+  },
+  MVA: {
+    name: "megavolt-ampere",
+    toAnchor: 1_000_000,
+    dimension: "apparentPower"
+  },
+  GVA: {
+    name: "gigavolt-ampere",
+    toAnchor: 1_000_000_000,
+    dimension: "apparentPower"
+  },
+};
