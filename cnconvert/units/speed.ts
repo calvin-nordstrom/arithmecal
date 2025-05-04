b30dda6afc3134facbe0d90ad651d4ae163356a6
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 13:02:08 -0400

    feat: added unit definition

---
+++ cnconvert/units/speed.ts
@@ -0,0 +1,39 @@
+import { UnitDefinition } from "../unit";
+
+export const speedUnits: Record<string, UnitDefinition> = {
+  "m/s": {
+    name: "meter per second",
+    toAnchor: 1,
+    dimension: "speed"
+  },
+  "km/h": {
+    name: "kilometer per hour",
+    toAnchor: 1000 / 3600,
+    dimension: "speed"
+  },
+  "mm/h": {
+    name: "millimeter per hour",
+    toAnchor: 0.001 / 3600,
+    dimension: "speed"
+  },
+  mph: {
+    name: "mile per hour",
+    toAnchor: 1609.344 / 3600,
+    dimension: "speed"
+  },
+  "ft/s": {
+    name: "foot per second",
+    toAnchor: 0.3048,
+    dimension: "speed"
+  },
+  "in/h": {
+    name: "inch per hour",
+    toAnchor: 0.0254 / 3600,
+    dimension: "speed"
+  },
+  knot: {
+    name: "knot",
+    toAnchor: 1852 / 3600,
+    dimension: "speed"
+  },
+};
