517e3a00ee3c853c31db087cadf76b5fbd6f22a4
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 13:01:33 -0400

    feat: added unit definition

---
+++ cnconvert/units/current.ts
@@ -0,0 +1,24 @@
+import { UnitDefinition } from "../unit";
+
+export const currentUnits: Record<string, UnitDefinition> = {
+  A: {
+    name: "ampere",
+    toAnchor: 1,
+    dimension: "current"
+  },
+  mA: {
+    name: "milliampere",
+    toAnchor: 0.001,
+    dimension: "current"
+  },
+  uA: {
+    name: "microampere",
+    toAnchor: 1e-6,
+    dimension: "current"
+  },
+  kA: {
+    name: "kiloampere",
+    toAnchor: 1000,
+    dimension: "current"
+  },
+};
