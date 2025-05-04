3494328a157fa94be80a1c5400da81cef0593020
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 13:01:47 -0400

    feat: added unit definition

---
+++ cnconvert/units/illuminance.ts
@@ -0,0 +1,19 @@
+import { UnitDefinition } from "../unit";
+
+export const illuminanceUnits: Record<string, UnitDefinition> = {
+  lx: {
+    name: "lux",
+    toAnchor: 1,
+    dimension: "illuminance"
+  },
+  ph: {
+    name: "phot",
+    toAnchor: 10_000,
+    dimension: "illuminance"
+  },
+  fc: {
+    name: "foot-candle",
+    toAnchor: 10.76391,
+    dimension: "illuminance"
+  },
+};
