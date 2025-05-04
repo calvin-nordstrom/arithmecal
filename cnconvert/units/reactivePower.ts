b33d36b8d74e765929efa16bc00d7853272e0f7c
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 13:02:05 -0400

    feat: added unit definition

---
+++ cnconvert/units/reactivePower.ts
@@ -0,0 +1,29 @@
+import { UnitDefinition } from "../unit";
+
+export const reactivePowerUnits: Record<string, UnitDefinition> = {
+  VAR: {
+    name: "volt-ampere reactive",
+    toAnchor: 1,
+    dimension: "reactivePower"
+  },
+  mVAR: {
+    name: "millivolt-ampere reactive",
+    toAnchor: 0.001,
+    dimension: "reactivePower"
+  },
+  kVAR: {
+    name: "kilovolt-ampere reactive",
+    toAnchor: 1000,
+    dimension: "reactivePower"
+  },
+  MVAR: {
+    name: "megavolt-ampere reactive",
+    toAnchor: 1_000_000,
+    dimension: "reactivePower"
+  },
+  GVAR: {
+    name: "gigavolt-ampere reactive",
+    toAnchor: 1_000_000_000,
+    dimension: "reactivePower"
+  },
+};
