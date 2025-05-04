213432957931838c4870aa24cdddccadff99b6d2
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-04 14:26:35 -0400

    refactor: updated unit index list

--- cnconvert/units/index.ts
+++ cnconvert/units/index.ts
@@ -1,13 +1,57 @@
+import { accelerationUnits } from "./acceleration";
+import { angleUnits } from "./angle";
+import { apparentPowerUnits } from "./apparentPower";
 import { areaUnits } from "./area";
+import { chargeUnits } from "./charge";
+import { currentUnits } from "./current";
+import { digitalUnits } from "./digital";
+import { energyUnits } from "./energy";
+import { forceUnits } from "./force";
+import { frequencyUnits } from "./frequency";
+import { illuminanceUnits } from "./illuminance";
 import { lengthUnits } from "./length";
+import { massUnits } from "./mass";
+import { massFlowRateUnits } from "./massFlowRate";
+import { paceUnits } from "./pace";
+import { partsPerUnits } from "./partsPer";
+import { powerUnits } from "./power";
 import { pressureUnits } from "./pressure";
+import { reactiveEnergyUnits } from "./reactiveEnergy";
+import { reactivePowerUnits } from "./reactivePower";
+import { speedUnits } from "./speed";
 import { temperatureUnits } from "./temperature";
+import { timeUnits } from "./time";
+import { torqueUnits } from "./torque";
+import { voltageUnits } from "./voltage";
 import { volumeUnits } from "./volume";
+import { volumeFlowRateUnits } from "./volumeFlowRate";
 
 export const allUnits = {
+  ...accelerationUnits,
+  ...angleUnits,
+  ...apparentPowerUnits,
   ...areaUnits,
+  ...chargeUnits,
+  ...currentUnits,
+  ...digitalUnits,
+  ...energyUnits,
+  ...forceUnits,
+  ...frequencyUnits,
+  ...illuminanceUnits,
   ...lengthUnits,
+  ...massUnits,
+  ...massFlowRateUnits,
+  ...paceUnits,
+  ...partsPerUnits,
+  ...powerUnits,
   ...pressureUnits,
+  ...reactiveEnergyUnits,
+  ...reactivePowerUnits,
+  ...speedUnits,
   ...temperatureUnits,
+  ...timeUnits,
+  ...torqueUnits,
+  ...voltageUnits,
   ...volumeUnits,
+  ...volumeFlowRateUnits,
 };
