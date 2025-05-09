7966e241af8d958e6d02f2bdba2aadba8d690142
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-09 15:50:27 -0400

    refactor: optimize SEO

--- app/conversion/decimal-to-ascii/page.tsx
+++ app/conversion/decimal-to-ascii/page.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Decimal to ASCII Conversion',
-  description: 'Arithmecal decimal to ASCII conversion',
-  keywords: '',
+  description: 'Convert decimal numbers to ASCII characters and vice versa with Arithmecal’s easy-to-use tool. Learn decimal-ASCII mappings and through conversion examples.',
+  keywords: 'decimal to ASCII, ASCII to decimal, decimal ASCII converter, ASCII conversion tool, number to character conversion, Arithmecal, base-10 to ASCII',
 };
 
 export default function DecimalToASCIIConversionPage() {
