28a81b702cd2efa0092f14446c05d654a9d91887
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-09 15:50:22 -0400

    refactor: optimize SEO

--- app/conversion/binary-to-hex/page.tsx
+++ app/conversion/binary-to-hex/page.tsx
@@ -5,8 +5,8 @@ import BinaryToHexConverter from '@/app/components/conversion/BinaryToHexConvert
 
 export const metadata: Metadata = {
   title: 'Binary to Hex Conversion',
-  description: 'Arithmecal binary to hex conversion',
-  keywords: '',
+  description: 'Convert binary numbers to hexadecimal and vice versa using Arithmecal’s calculator. Understand 4-bit binary to hex mapping,  examples, and its importance.',
+  keywords: 'binary to hex, hex to binary, binary converter, hexadecimal calculator, base-2 to base-16, hex digits, binary hex conversion, 4-bit binary mapping, hex values in computing',
 }
 
 export default function BinaryToHexConversionPage() {
