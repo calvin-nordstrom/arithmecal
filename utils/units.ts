69c483886a3df60a39c2f4b2435da7bc584e1fb5
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-13 17:49:33 -0400

    refactor: update to render caret notation

--- utils/units.ts
+++ utils/units.ts
@@ -1,6 +1,9 @@
 export const formatUnitDisplay = (abbr: string): string => {
-  return abbr.replace(/(\d)/g, (match) => {
-    const superscripts = '⁰¹²³⁴⁵⁶⁷⁸⁹';
-    return superscripts[parseInt(match)];
-  });
+  const superscripts = '⁰¹²³⁴⁵⁶⁷⁸⁹';
+  abbr = abbr.replace(/\^(\d)/g, (_, digit) => superscripts[parseInt(digit)]);
+
+  const subscripts = '₀₁₂₃₄₅₆₇₈₉';
+  abbr = abbr.replace(/_(\d)/g, (_, digit) => subscripts[parseInt(digit)]);
+
+  return abbr;
 };
