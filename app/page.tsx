865c487531988a1dfd03c22e61d70055ed4067cd
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-22 19:22:33 -0400

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,65 @@
-import Link from 'next/link'
+import type { Metadata } from 'next'
+import Link from 'next/link';
+import Footer from './components/Footer';
+
+export const metadata: Metadata = {
+  title: 'Arithmecal',
+  description: '',
+  keywords: ''
+}
 
 export default function Home() {
   return (
-    <main>
-      <h1>Arithmecal</h1>
-      <div>
-        {/* <Link href='/math/area'>Area Calculator</Link> */}
+    <>
+      <main className='home'>
+        <div className='title-banner'>
+          <h1>Arithmecal</h1>
+          <h2><em>The everything calculator</em></h2>
+        </div>
+
+        <div className='home-content-container'>
+          <div className='home-content'>
+            <h2>Search Calculators</h2>
+            <input type='text' className='search-input' placeholder='Search calculators...' />
+          </div>
+          <div className='home-content'>
+            <h2>Featured Categories</h2>
+            <div className='home-category-item-container'>
+              <div className='home-category-item'>
+                <Link href='./conversion' className='home-category-link'>
+                  <h3>Conversion</h3>
+                </Link>
+              </div>
+              <div className='home-category-item'>
+                <Link href='./math' className='home-category-link'>
+                  <h3>Math</h3>
+                </Link>
+              </div>
+              <div className='home-category-item'>
+                <Link href='./physics' className='home-category-link'>
+                  <h3>Physics</h3>
+                </Link>
+              </div>
+              <div className='home-category-item'>
+                <Link href='./reference' className='home-category-link'>
+                  <h3>Reference</h3>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className='home-content home-about'>
+            <h2>About Us</h2>
+            <p>Whether you&apos;re a student, professional, or casual learner, calculations are involved. Sometimes you might not know how to solve something or lack the details behind a calculation. These are times where a tool like Arithmecal is your best friend. Our services provide accurate results in an easy-to-use and efficient calculator format.</p>
+            <Link href='./about' className='home-about-link'>
+              <span>Read more</span>
+            </Link>
+          </div>
+        </div>
+      </main>
+
+      <div className='bottom'>
+        <Footer />
       </div>
-    </main>
+    </>
   )
 }
