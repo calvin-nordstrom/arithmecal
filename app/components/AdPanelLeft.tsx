60ad32acfacae46567b0c06f5f691a04ee18fda7
Calvin Nordstrom <calnordstrom@icloud.com>
2025-05-29 16:22:39 -0400

    refactor: remove layout testing tags

--- app/components/AdPanelLeft.tsx
+++ app/components/AdPanelLeft.tsx
@@ -5,15 +5,7 @@ import React from 'react';
 export default function AdPanelLeft() {
   return (
     <div className='ad-panel-left'>
-      <div style={{width: '150px', height: '150px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
-      <div style={{width: '200px', height: '150px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
-      <div style={{width: '150px', height: '150px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
+      
     </div>
   );
 }
--- app/components/AdPanelRight.tsx
+++ app/components/AdPanelRight.tsx
@@ -5,15 +5,7 @@ import React from 'react';
 export default function AdPanelRight() {
   return (
     <div className='ad-panel-right'>
-      <div style={{width: '150px', height: '150px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
-      <div style={{width: '100px', height: '150px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
-      <div style={{width: '200px', height: '300px', backgroundColor: 'gray'}}>
-        <p>This is an advertisement!</p>
-      </div>
+      
     </div>
   );
 }
