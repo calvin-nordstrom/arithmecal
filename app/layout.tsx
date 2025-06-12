75f9597f92f02b884d9f50aae34fedef6cfab712
Calvin Nordstrom <calnordstrom@icloud.com>
2025-06-12 12:07:21 -0400

    refactor: remove redundant metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
-export const metadata: Metadata = {
-  title: 'Arithmecal',
-  description: 'Arithmecal home page',
-  keywords: '',
-}
 
 export default function RootLayout({
   children,
